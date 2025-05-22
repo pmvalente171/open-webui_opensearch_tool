321ce1c0194adc2535045d10e07c08eccdff983e
Pedro Valente <pm.valente@campus.fct.unl.pt>
2025-05-23 00:49:24 +0100

    Tool can now preform queries

---
+++ opensearch_playground/__main__.py
@@ -0,0 +1,79 @@
+﻿#!/usr/bin/env python3
+"""
+OpenSearch Client Script
+A simple script to connect to OpenSearch and perform searches based on user input.
+"""
+
+from opensearchpy import OpenSearch
+
+import dotenv
+
+env_vars = dotenv.dotenv_values()
+_user_id = env_vars['USER_ID']
+_user_password = env_vars['USER_PASSWORD']
+
+class ConfigOpenSearch:
+
+    host = 'api.novasearch.org'
+    port = 443
+    index_name = "farfetch_images"
+    user = _user_id  # Add your username here.
+    password = _user_password  # Add your user password here. For testing only. Don't store credentials in code.
+    source = ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender',
+               'product_main_colour', 'product_second_color', 'product_brand', 'product_materials',
+               'product_short_description', 'product_attributes', 'product_image_path',
+               'product_highlights', 'outfits_ids', 'outfits_products']
+    source_with_image_embedding = source + ['image_embedding']
+
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,
+        http_auth=(user, password),
+        url_prefix='opensearch',
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False
+    )
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ConfigOpenSearch, cls).__new__(cls)
+        return cls.instance
+
+
+def main():
+    """
+    :return:
+    """
+
+    opensearch = ConfigOpenSearch()
+
+    # Example search query
+    search_query = "blue jeans"
+
+    # Example search body
+    query = {
+        'size': 5,
+        '_source': opensearch.source,
+        "query": {
+            "match": {
+                "product_short_description": search_query
+            }
+        }
+    }
+
+    # Perform the search
+    response = opensearch.client.search(
+        body=query,
+        index=opensearch.index_name
+    )
+
+    # Print the response
+    print("Search Response:")
+    for hit in response['hits']['hits']:
+        print(f"ID: {hit['_id']}, Source: {hit['_source']}")
+
+
+if __name__ == "__main__":
+    main() # TODO: It's working !!!
--- webui_opensearch.py
+++ webui_opensearch.py
@@ -10,6 +10,11 @@ version: 0.0.1
 licence: MIT
 """
 
+import os
+import dotenv
+
+from opensearchpy import OpenSearch
+from pydantic import BaseModel, Field
 
 class Tools:
     class Valves(BaseModel):
@@ -19,7 +24,27 @@ class Tools:
             description="OpenSearch endpoint URL. Default is http://localhost:9200",
         )
 
-        DATABASE_DESCRIPTION : str = Field(
+        OPEN_SEARCH_USERNAME : str = Field(
+            default="admin",
+            description="OpenSearch username. Default is 'admin'",
+        )
+
+        OPEN_SEARCH_PASSWORD : str = Field(
+            default="admin",
+            description="OpenSearch password. Default is 'admin'",
+        )
+
+        OPEN_SEARCH_PORT : int = Field(
+            default=9200,
+            description="OpenSearch port. Default is 9200",
+        )
+
+        OPEN_URL_PREFIX : str = Field(
+            default="opensearch",
+            description="OpenSearch URL prefix. Default is 'opensearch'",
+        )
+
+        DESCRIPTION : str = Field(
             default="A collection of documents stored in OpenSearch.",
             description="Description of the database. Default is 'A collection of documents stored in OpenSearch.'",
         )
@@ -28,10 +53,71 @@ class Tools:
         """Initialize the tool"""
         self.valves = self.Valves()
 
-    def query_opensearch(self, query : str) -> dict:
+        # TODO: debug stuff
+        env_vars = dotenv.dotenv_values()
+        user = env_vars['USER_ID']
+        password = env_vars['USER_PASSWORD']
+
+        host = env_vars['HOST']
+        port = env_vars['PORT']
+
+        self.client = OpenSearch(
+            hosts=[{'host': host, 'port': port}],
+            http_compress=True,
+            http_auth=(user, password),
+            url_prefix='opensearch',
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False
+        )
+
+        # TODO: More debug stuff
+        self.source = ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender',
+               'product_main_colour', 'product_second_color', 'product_brand', 'product_materials',
+               'product_short_description', 'product_attributes', 'product_image_path',
+               'product_highlights', 'outfits_ids', 'outfits_products']
+
+
+    def query_opensearch(self, query : str) -> str:
         """
         Query the OpenSearch index and return the results.
         :param query: The query string to search for.
         :return: The search results as a string.
         """
 
+        # Search body
+        search_body = {
+            'size': 5, # TODO: Make this parameter editable
+            '_source': self.source,
+            "query": {
+                "match": {
+                    "product_short_description": query
+                }
+            }
+        }
+
+        # Perform the search
+        response = self.client.search(index="farfetch_images", body=search_body)
+
+        # Extract the hits from the response
+        hits = response['hits']['hits']
+
+        # Return the hits as a string
+        return str(hits)
+
+    def get_tool_info(self) -> dict:
+        """
+        Get the tool information.
+        :return: A dictionary containing the tool information.
+        """
+
+        return {
+            "name": "OpenSearch Tool",
+            "description": self.valves.DESCRIPTION,
+            "endpoint": self.valves.OPEN_SEARCH_ENDPOINT,
+            "username": self.valves.OPEN_SEARCH_USERNAME,
+            "password": self.valves.OPEN_SEARCH_PASSWORD,
+            "port": self.valves.OPEN_SEARCH_PORT,
+            "url_prefix": self.valves.OPEN_URL_PREFIX
+        }
